05f78e426fdf22ee4fffda958ecce62d0b48a328
BetLopGlz <84944622+BetLopGlz@users.noreply.github.com>
2021-10-01 14:08:40 -0500

    agregando formulario de reservaciones

--- src/componentes/reservaciones/FormularioReservaciones.js
+++ src/componentes/reservaciones/FormularioReservaciones.js
@@ -1,57 +1,108 @@
-<form className="mb-5">
-  <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-    <Form.Label column sm={2}>
-      Email
-    </Form.Label>
-    <Col sm={10}>
-      <Form.Control type="email" placeholder="Email" />
-    </Col>
-  </Form.Group>
+import React, {Fragment,useState}  from 'react';
+import '../../App.css';
 
-  <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-    <Form.Label column sm={2}>
-      Password
-    </Form.Label>
-    <Col sm={10}>
-      <Form.Control type="password" placeholder="Password" />
-    </Col>
-  </Form.Group>
-  <fieldset>
-    <Form.Group as={Row} className="mb-3">
-      <Form.Label as="legend" column sm={2}>
-        Radios
-      </Form.Label>
-      <Col sm={10}>
-        <Form.Check
-          type="radio"
-          label="first radio"
-          name="formHorizontalRadios"
-          id="formHorizontalRadios1"
-        />
-        <Form.Check
-          type="radio"
-          label="second radio"
-          name="formHorizontalRadios"
-          id="formHorizontalRadios2"
-        />
-        <Form.Check
-          type="radio"
-          label="third radio"
-          name="formHorizontalRadios"
-          id="formHorizontalRadios3"
-        />
-      </Col>
-    </Form.Group>
-  </fieldset>
-  <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
-    <Col sm={{ span: 10, offset: 2 }}>
-      <Form.Check label="Remember me" />
-    </Col>
-  </Form.Group>
 
-  <Form.Group as={Row} className="mb-3">
-    <Col sm={{ span: 10, offset: 2 }}>
-      <Button type="submit">Sign in</Button>
-    </Col>
-  </Form.Group>
-</form>
+
+
+const FormularioReservaciones = () =>    {
+  const styleB={
+    backgroundColor:'#ea8118',
+  borderColor:'#ea8118'
+  }  
+
+  const [cita, actualizarCita] = useState({
+    nombre: '',
+    email: '',
+    telefono: '',
+    fecha: '',
+   hora: '',
+   noPersonas: '',
+   indicaciones: '',
+});
+const [ error, actualizarError ] = useState(false)
+
+// Función que se ejecuta cada que el usuario escribe en un input
+const actualizarState = e => {
+  actualizarCita({
+    ...cita,
+    [e.target.name]: e.target.value 
+ })
+}
+
+// Extraer los valores
+const { nombre,email,telefono,fecha,hora,noPersonas,indicaciones } = cita;
+const submitCita = e => {
+  e.preventDefault();
+ 
+// Validar
+if(nombre.trim() === '' || email.trim() === ''  || telefono.trim() === ''  || fecha.trim() === ''  || hora.trim() === '' || noPersonas.trim()===''|| indicaciones.trim()==='' ){
+  actualizarError(true);
+  return;
+}
+
+// Eliminar el mensaje previo 
+actualizarError(false);
+  console.log(cita)
+
+  // Reiniciar el form
+  actualizarCita({
+    nombre: '',
+    email: '',
+    telefono: '',
+    fecha: '',
+   hora: '',
+   noPersonas: '',
+   indicaciones: '',
+})
+}
+
+  return (
+    <Fragment>
+    { error? <p className="alerta-error">Todos los campos son obligatorios</p> : null }
+      <form className="mb-4" onSubmit={submitCita}>
+
+        <div className="form-group">
+          <label htmlFor="nya" className="negrita">Nombre y Apellidos</label>
+          <input type="text" className="form-control" name="nombre" required onChange={actualizarState} value={nombre}/>            
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email" className="negrita">Email</label>
+          <input type="email" className="form-control" id="email" name="email" required onChange={actualizarState} value={email}/> 
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="telefono" className="negrita">Telefono</label>
+          <input type="text" className="form-control" id="telefono" name="telefono" required onChange={actualizarState} value={telefono} />
+        </div>
+
+        <div><label htmlFor="fecha" className="negrita">Fecha:</label>
+        <input type="date" className="form-control" className="form-control" id="fecha" name="fecha" required onChange={actualizarState} value={fecha}/>
+        </div> 
+
+        <div className="form-group">
+          <label htmlFor="hora" className="negrita">Hora</label>
+          <input type="time" name="hora" className="form-control" required onChange={actualizarState} value={hora}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="noPersonas" className="negrita" required>No. de Personas</label>
+          <input type="number" name="noPersonas" className="form-control" required onChange={actualizarState} value={noPersonas}/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="indicaciones" className="negrita">Indicaciones Especiales</label>
+          <textarea className="form-control" id="indicaciones" name="indicaciones" onChange={actualizarState} value={indicaciones}></textarea>
+        </div>
+
+        <button type="submit" style={styleB} className="btn btn-dark">Reservar</button>
+
+      </form>
+      </Fragment>
+  )
+  
+}
+
+   
+
+
+export default FormularioReservaciones;
